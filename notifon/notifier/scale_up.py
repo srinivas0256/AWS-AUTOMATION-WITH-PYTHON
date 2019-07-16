1b28d7cb51bf835055a9f40bc24afae5afe6da1e
EC2 Default User <ec2-user@ip-172-31-85-64.ec2.internal>
2019-07-16 06:58:54 +0000

    pushing notifier directory

---
+++ notifon/notifier/scale_up.py
@@ -0,0 +1,5 @@
+import boto3
+session = boto3.Session(profile_name='pythonAutomation1')
+as_client = session.client('autoscaling')
+
+as_client.execute_policy(AutoScalingGroupName='Notifon Example Group', PolicyName='Scale Up')
