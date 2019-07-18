0abe815cd0cc43c41fc0b2fef09c40c8dbe245e5
srinivas0256 <48003682+srinivas0256@users.noreply.github.com>
2019-07-18 16:07:36 +0530

    rekognition intro

---
+++ Rekognition/rekognition.py
@@ -0,0 +1,43 @@
+ import boto3
+
+  session = boto3.Session(profile_name='pythonAutomation1')
+
+  s3 = session.resource('s3')
+
+  bucket = s3.create_bucket(Bucket='robinvideolyzervideos', CreateBucketConfiguration={'LocationConstraint': session.region_name})
+
+  from pathlib import Path
+
+  get_ipython().run_line_magic('ls', '/home/ec2-user/watch?v=668nUCeBHyY')
+
+  pathname = '/home/ec2-user/watch?v=668nUCeBHyY'
+
+  path = Path(pathname).expanduser().resolve()
+
+  print(path)
+
+  bucket.upload_file(str(path), str(path.name))
+
+  rekognition_client = session.client('rekognition')
+
+  response = rekognition_client.start_label_detection(Video={'S3Object': { 'Bucket': bucket.name, 'Name': path.name}})
+
+  response
+
+  job_id = response['JobId']
+
+  result = rekognition_client.get_label_detection(JobId=job_id)
+
+  result
+
+  result.keys()
+
+  result['JobStatus']
+
+  result['ResponseMetadata']
+
+  result['VideoMetadata']
+
+  result['Labels']
+
+  len(result['Labels'])
